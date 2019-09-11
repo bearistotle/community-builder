3ce1bb6d96b2bd47084eef112cd81fb863966494
Brandon Walker <brandonwalker215@gmail.com>
2019-09-11 06:09:04 -0500

    "Updated availability index template to account for change to the name of the id field after refactoring Availability and Event classes; updated add availability template to account for the removal of the location field and the addition of the name and description fields in the Availability class."

--- src/main/java/dev/bearistotle/communitybuilder/controllers/UserController.java
+++ src/main/java/dev/bearistotle/communitybuilder/controllers/UserController.java
@@ -1,5 +1,6 @@
 package dev.bearistotle.communitybuilder.controllers;
 
+import dev.bearistotle.communitybuilder.models.Availability;
 import dev.bearistotle.communitybuilder.models.HashUtils;
 import dev.bearistotle.communitybuilder.models.User;
 import dev.bearistotle.communitybuilder.models.data.UserDao;
@@ -14,7 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.lang.Character;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
@@ -34,8 +41,59 @@ public class UserController {
         }
         // get user from session
         User user = userDao.findByEmail((String) session.getAttribute("user"));
+        // TODO: Get and pass in items for each day of the calendar, with each day having a list for each hour. Also,
+        //   pass in 24 as "hours"
+        // List of HashMaps with hour as key and list of Availabilities during that hour as value for each day
+        HashMap<Integer,ArrayList<Availability>> monItems = new HashMap<>();
+        HashMap<Integer,ArrayList<Availability>> tuesItems = new HashMap<>();
+        HashMap<Integer,ArrayList<Availability>> wedItems = new HashMap<>();
+        HashMap<Integer,ArrayList<Availability>> thursItems = new HashMap<>();
+        HashMap<Integer,ArrayList<Availability>> friItems = new HashMap<>();
+        HashMap<Integer,ArrayList<Availability>> satItems = new HashMap<>();
+        HashMap<Integer,ArrayList<Availability>> sunItems = new HashMap<>();
+
+
+        ArrayList<HashMap<Integer,ArrayList<Availability>>> dayMaps = new ArrayList<>();
+        dayMaps.add(monItems);
+        dayMaps.add(tuesItems);
+        dayMaps.add(wedItems);
+        dayMaps.add(thursItems);
+        dayMaps.add(friItems);
+        dayMaps.add(satItems);
+        dayMaps.add(sunItems);
+
+        for (HashMap<Integer,ArrayList<Availability>> dayMap: dayMaps) {
+
+            // Create a map entry for each hour of the day
+            for (int i = 1; i < 25; i++) {
+                ArrayList<Availability> emptyList = new ArrayList<>();
+                dayMap.put(i, emptyList);
+
+            }
+        }
+
+        for (DayOfWeek day: DayOfWeek.values()) {
+            HashMap<Integer,ArrayList<Availability>> dayMap = dayMaps.get(day.getValue() - 1);
+
+            for (Availability availability : user.getAvailabilities()) {
+                if (availability.getDate()
+                        .getDayOfWeek()
+                        .getDisplayName(TextStyle.FULL, Locale.getDefault())
+                        .equals(day.getDisplayName(TextStyle.FULL, Locale.getDefault()))){
+                    // Add Availability to ArrayList that corresponds to the hour of its startTime in the HashMap for
+                    // the day.
+                    ArrayList<Availability> hourList = dayMap.get(availability.getStartTime().getHour());
+                    hourList.add(availability);
+                }
+            }
+        }
+
+        int[] hours = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+
         model.addAttribute("user", user);
         model.addAttribute("title", "Users");
+        model.addAttribute("hours", hours);
+        model.addAttribute("sundayItems", sunItems);
 
         return "user/index";
     }
@@ -168,7 +226,7 @@ public class UserController {
             if (HashUtils.checkPassword(password, registeredUser.getPwHash())) {
                 session.setAttribute("user", registeredUser.getEmail());
 
-                return "user/index";
+                return "redirect:";
             }
         }
         // if login fails, return to login screen with error message
